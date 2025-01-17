103437ac4b98827fd45479d2487221c25a4d7bd4
Thiago Felix <thiagof@tutamail.com>
2025-01-17 23:48:52 +0000

    hotfix: correcting grammar

--- app/cli/filebunker.py
+++ app/cli/filebunker.py
@@ -12,9 +12,9 @@ os.environ["ROOT"] = ROOT
 from api.classlib.db import DataBase
 from api.classlib.jsonhelp import JsonHelp
 
-#JsonHelp.create_config_file(config_file_name="1.json", password="Thiagoak103")
+#JsonHelp.create_config_file(config_file_name="1.json", password="")
 
-salt, iv, key, data = JsonHelp.decrypt_file(file_path='data/1.json', password="Thiagoak103")
+salt, iv, key, data = JsonHelp.decrypt_file(file_path='data/1.json', password="12344")
 db = DataBase(data, salt, iv, key)
-#db.db["paths"]["/home/usr/Downloads"] = {"teste.txt":"Teucu"}
+#db.db["paths"]["/home/usr/Downloads"] = {"teste.txt":"Teu Ceu"}
 #db.save()
