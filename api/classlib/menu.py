5e782163db3b0082c8e9145f0fc0b1e9fe2dde01
thiago-dev-cyber <thiagof@tutamail.com>
2025-01-27 00:04:33 -0300

    feat(menu): Implementing the menu and adding an animation.

--- api/classlib/menu.py
+++ api/classlib/menu.py
@@ -1,132 +1,181 @@
-import os
-import sys
+class Menu:
+    first_menu = """
 
+==========================================
+                 MENU
+==========================================
 
-# Ensure parent directory is in sys.path for imports
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+1 - Load config file.
+2 - Create a new config file. 
+0 - Exit.
+"""
 
-# Import File and FileHelp classes
-from api.classlib.file import File
-from api.classlib.filehelp import FileHelp
+    main_menu = """
+==========================================
+                 MENU
+==========================================
+Choose an option:
 
+1 - Upload files.
+2 - Get files.
+3 - Add a new path.
+0 - exits"""
 
-class Manager:
-    """
-    Class responsible for managing local files as well as remote files.
-    """
+    anim_01 = """
+             ________________________________________________
+            /                                                \\
+           |    _________________________________________     |
+           |   |                                         |    |
+           |   |  user@debian:~$ _                       |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |_________________________________________|    |
+           |                                                  |
+            \\_________________________________________________/
+                   \\___________________________________/
+                ___________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
+:-------------------------------------------------------------------------:
+`---._.-------------------------------------------------------------._.---'
+                              -Thiago Felix-
+"""
 
-    pool_files = {}
+    anim_02 = """
+             ________________________________________________
+            /                                                \\
+           |    _________________________________________     |
+           |   |                                         |    |
+           |   |  user@debian:~$                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |_________________________________________|    |
+           |                                                  |
+            \\_________________________________________________/
+                   \\___________________________________/
+                ___________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
+:-------------------------------------------------------------------------:
+`---._.-------------------------------------------------------------._.---'
+                              -Thiago Felix-
+"""
 
-    @classmethod
-    def add_path(cls, path: str, database: object):
-        """
-        Method responsible for adding a new directory path to a JSON config file.
+    anim_03 = """
+             ________________________________________________
+            /                                                \\
+           |    _________________________________________     |
+           |   |                                         |    |
+           |   |  user@debian:~$ cd /filebunker          |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |_________________________________________|    |
+           |                                                  |
+            \\_________________________________________________/
+                   \\___________________________________/
+                ___________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
+:-------------------------------------------------------------------------:
+`---._.-------------------------------------------------------------._.---'
+                              -Thiago Felix-
+"""
+    anim_04 = """
+             ________________________________________________
+            /                                                \\
+           |    _________________________________________     |
+           |   |                                         |    |
+           |   |  user@debian:~$ cd /filebunker          |    |
+           |   |  user@debian:~/filebunker  ./start      |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |_________________________________________|    |
+           |                                                  |
+            \\_________________________________________________/
+                   \\___________________________________/
+                ___________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
+:-------------------------------------------------------------------------:
+`---._.-------------------------------------------------------------._.---'
+                              -Thiago Felix-
+"""
 
-        Args:
-            path (str): Path that will be added to the configuration file.
-            database (object): Configuration file instance.
-        """
-        try:
-            if not os.path.exists(path):
-                raise Exception(f'The directory at {path} does not exist.')
-
-            if path not in database.db['paths']:
-                database.db['paths'][path] = {}
-
-                files = Manager._list_files(path)
-
-                if files is not None:
-                    cls.pool_files[path] = files
-                    database.db['paths'][path] = files
-                    # print(cls.pool_files)
-
-                database.save()
-
-            else:
-                raise Exception('The directory has already been added.')
-
-        except Exception as err:
-            print(f'There was an error trying to add the path: {err}')
-
-    @staticmethod
-    def upload_files():
-        pass
-
-    @staticmethod
-    def download_files():
-        pass
-
-    @staticmethod
-    def _list_files(path: str) -> dict:
-        try:
-            if not os.path.exists(path):
-                raise NotADirectoryError(f'The directory at {path} does not exist.')
-
-            files = {}
-            # {database.txt: {id:1, cksum: afaf1f3f1, loaded:false}}
-            for file in os.listdir(path):
-                file_path = os.path.join(path, file)
-                if os.path.isfile(file_path):
-                    if file not in Manager.pool_files:
-                        files[file] = {
-                            'id': FileHelp.gen_file_id(),
-                            'cksum': FileHelp.cksum(file_path),
-                            'real_path': file_path,
-                            'loaded': False,
-                        }
-
-            return files
-
-        except NotADirectoryError as err:
-            print(f'There was an error trying to list files: {err}')
-
-        except Exception as err:
-            print(f'{err}')
-
-        return files
-
-    @classmethod
-    def reload_files(cls, db: object):
-        """
-        Reloads files into memory from JSON.
-
-        Args:
-            db (object): Database Instance.
-
-        Returns:
-            None
-        """
-        try:
-            cls.pool_files = db.db['paths']
-
-        except Exception as err:
-            print(f'Unable to reload files: {err}')
-
-    @classmethod
-    def _load_file(cls, file_path: str) -> object:
-        """
-        Method responsible for uploading a file when it is requested.
-
-        Args:
-            file_path (str): Full path of the file that will be uploaded.
-        """
-        try:
-            file_name = os.path.basename(file_path)
-            file_path = os.path.dirname(file_path)
-
-            if file_name not in cls.pool_files[file_path]:
-                raise Exception(f'File not Found {file_name}')
-
-            file_info = cls.pool_files[file_path][file_name]
-
-            file = File(
-                id=file_info['id'],
-                name=file_name,
-                cksum=file_info['cksum'],
-                path=file_info['real_path'],
-            )
-
-            return file
-
-        except Exception as err:
-            print(f'Unable to upload the file: {err}')
+    anim_05 = """
+             ________________________________________________
+            /                                                \\
+           |    _________________________________________     |
+           |   |                                         |    |
+           |   | LOADING...                              |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |                                         |    |
+           |   |_________________________________________|    |
+           |                                                  |
+            \\_________________________________________________/
+                   \\___________________________________/
+                ___________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
+:-------------------------------------------------------------------------:
+`---._.-------------------------------------------------------------._.---'
+                              -Thiago Felix-
+"""
--- app/cli/filebunker.py
+++ app/cli/filebunker.py
@@ -1,39 +1,141 @@
 import inspect
 import os
 import sys
+from getpass import getpass
+from time import sleep
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-# Get a directory ROOT
+# Get the ROOT directory
 CURRENTDIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 ROOT = os.path.dirname(os.path.dirname(CURRENTDIR))
 
-# Adding a ROOT directory a sys.path
+# Adding the ROOT directory to sys.path
 sys.path.append(ROOT)
 os.environ['ROOT'] = ROOT
 
-from api.classlib.db import DataBase
-from api.classlib.menu import Manager
+from api.classlib.connect_mega import ConnectMega
+from api.classlib.manager import Manager
+from api.classlib.menu import Menu
 
 
-# JsonHelp.create_config_file(config_file_name='1.json', password='1234')
-
-db = DataBase.load_db('data/1.json', '1234')
-# Manager.add_path('/home/thi/Downloads', db)
-
-# print(db.db)
+def clear_screen():
+    """Clears the terminal screen."""
+    os.system('clear || cls')
 
 
-Manager.reload_files(db)
-file = Manager._load_file('/home/thi/Downloads/password.txt')
+def animation():
+    """Displays the animated loading sequence."""
+    animations = [
+        Menu.anim_01,
+        Menu.anim_02,
+        Menu.anim_03,
+        Menu.anim_04,
+        Menu.anim_05,
+    ]
 
-print(file)
-email = os.getenv('email')
-password = os.getenv('password')
-# m = ConnectMega(email, password)
-# m.start()
-# print(Manager._list_files("/home/user/Downloads"))
-# db.save()
+    for anim in animations:
+        clear_screen()
+        print(anim)
+        sleep(1.50)
+
+
+def prompt_menu(menu_text, options):
+    """
+    Displays a menu and prompts the user for input.
+
+    Args:
+        menu_text (str): The text to display for the menu.
+        options (list[int]): List of valid options.
+
+    Returns:
+        int: The selected option.
+    """
+    while True:
+        try:
+            clear_screen()
+            print(menu_text)
+            choice = int(input('>> '))
+            if choice in options:
+                return choice
+            print(f'Invalid option! Please select one of {options}.')
+        except ValueError:
+            print('Invalid input! Please enter a number.')
+        except KeyboardInterrupt:
+            print('\nExiting...')
+            sys.exit()
+
+
+def main_menu(m):
+    """
+    Displays the main menu and handles file upload/download operations.
+
+    Args:
+        m (ConnectMega): The Mega connection object.
+    """
+    Manager.reload_files()
+
+    while True:
+        choice = prompt_menu(Menu.main_menu, [1, 2, 0])
+
+        if choice == 1:
+            print('Uploading files...')
+            Manager.upload_files(m)
+        elif choice == 2:
+            print('Downloading files...')
+            Manager.download_files(m)
+        elif choice == 0:
+            print('Returning to the main menu...')
+            break
+
+
+def configure_and_start():
+    """
+    Prompts the user to configure and start the application.
+    """
+    print('Enter the path to the configuration file:')
+    path = input('>> ').strip()
+    print('\nEnter the password:')
+    password = getpass('>> ')
+
+    clear_screen()
+    print('Loading configuration file...')
+    sleep(1)
+
+    if not Manager.init_database(path, password):
+        print('Failed to initialize the database. Please check your inputs.')
+        sleep(2)
+        return
+
+    email = os.getenv('email')
+    password = os.getenv('password')
+
+    m = ConnectMega(email, password)
+    m.start()
+
+    main_menu(m)
+
+
+def main():
+    """Main entry point for the program."""
+    # Uncomment below line to enable animation
+    animation()
+
+    while True:
+        choice = prompt_menu(Menu.first_menu, [0, 1, 2])
+
+        if choice == 0:
+            print('Exiting the program...')
+            break
+        elif choice == 1:
+            configure_and_start()
+        elif choice == 2:
+            print('Option not implemented yet.')
+            sleep(2)
+
+
+if __name__ == '__main__':
+    main()
